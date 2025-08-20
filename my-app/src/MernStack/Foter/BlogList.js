0dc5957fa82b7858cc890979b47d6644884495eb
Hassan-Ali11 <hassannazir1974@gmail.com>
2025-08-20 08:51:40 +0500

    Update BlogList.js

--- my-app/src/MernStack/Foter/BlogList.js
+++ my-app/src/MernStack/Foter/BlogList.js
@@ -1,7 +1,6 @@
 // import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
 // import { Link } from 'react-router-dom';
-
 // const BlogList = () => {
 //   const [blogs, setBlogs] = useState([]);
 //   const [loading, setLoading] = useState(true);
